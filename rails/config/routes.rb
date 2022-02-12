a1335978e517c0ddc115f0480b2f37dfccca1397
mountaincenter <y.hiroyuki560730@gmail.com>
2022-02-13 08:41:08 +0900

    create_rails7_env

---
+++ rails/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
+  root "posts#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
